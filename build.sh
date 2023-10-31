52db5b4ee7fd9d6064f929c43e44867f17765f91
Hiroshi Ukai <dakusui@gmail.com>
2023-11-01 07:12:41 +0900

    Document improvement (WIP).

--- build.sh
+++ build.sh
@@ -8,16 +8,8 @@ function main() {
   else
     # If being sourced.
     export -f commandunit
-    __commandunit_refresh_cloned_commandunit "--branch ${COMMANDUNIT_VERSION}" "" || return "${?}"
     echo "This file was sourced. Try 'commandunit --help' to see usage." >&2
   fi
 }
 
-export PROJECT_BASE_DIR="${PWD}"
-export COMMANDUNIT_SOURCE_DIR="${PROJECT_BASE_DIR}/src/dependencies/commandunit"
-export COMMANDUNIT_MINOR_VERSION="22"
-export COMMANDUNIT_VERSION="v1.${COMMANDUNIT_MINOR_VERSION}"
-export COMMANDUNIT_SNAPSHOT_VERSION="v1.$((${COMMANDUNIT_MINOR_VERSION} + 1))"
-# To workaround: https://github.com/dakusui/commandunit/issues/13
-export COMMANDUNIT_PWD="${PROJECT_BASE_DIR}"
 main "${@}"
