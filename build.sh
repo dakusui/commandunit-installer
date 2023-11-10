280bc3d70ea85fee46a291962fe8ea22716966ff
Hiroshi Ukai <dakusui@gmail.com>
2023-11-11 08:00:25 +0900

    Make commandunit function independent.

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 source "$(dirname "${BASH_SOURCE[0]}")/commandunit.rc"
 
 function main() {
+  # app="commandunit"
+  # rc="$HOME/.${app}rc" && \
+  # target="$HOME/.bashrc" && \
+  # tag="#-${app^^}" && \
+  # cat <(curl "https://raw.githubusercontent.com/dakusui/commandunit/main/src/site/adoc/resources/examples/function_definition.rc") > "${rc}" && \
+  # sh -c 'grep "$4" "$1" >& /dev/null || printf "source $3$2$3 $4\n" >> "$1"' - "${target}" "${rc}" '"' "${tag}"
+
   if [ "${BASH_SOURCE[0]}" == "$0" ]; then
     # If being executed.
     rm -fr target commandunit-out
-    commandunit --commandunit-dir=. -- "${@}"
+    commandunit --clean -- "${@}"
   else
     # If being sourced.
     export -f commandunit
