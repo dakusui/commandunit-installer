13134488d4a0dfae19162307adad693662956743
Hiroshi Ukai <dakusui@gmail.com>
2023-11-15 07:32:32 +0900

    Update the commandunit script.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-source "$(dirname "${BASH_SOURCE[0]}")/commandunit.rc"
+source "$(dirname "${BASH_SOURCE[0]}")/commandunit"
 
 function main() {
   # app="commandunit"
@@ -8,7 +8,8 @@ function main() {
   # cat <(curl "https://raw.githubusercontent.com/dakusui/commandunit/main/src/site/adoc/resources/examples/function_definition.rc") > "${rc}" && \
   # sh -c 'grep "$4" "$1" >& /dev/null || printf "source $3$2$3 $4\n" >> "$1"' - "${target}" "${rc}" '"' "${tag}"
 
-  if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+  cat <(https://dakusui.github.io/commandunit/resources/examples/commandunit) > commandunit
+    if [ "${BASH_SOURCE[0]}" == "$0" ]; then
     # If being executed.
     rm -fr target commandunit-out
     commandunit --clean -- "${@}"
