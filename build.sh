ad0c4a1b9b498fd25cfea0ac1ab12710524622af
Hiroshi Ukai <hiroshi.ukai@rakuten.com>
2023-11-16 06:58:41 +0900

    Improve build.sh (WIP)

--- build.sh
+++ build.sh
@@ -1,22 +1,17 @@
-source "$(dirname "${BASH_SOURCE[0]}")/commandunit"
+#!/usr/bin/env bash
+
+set -eu -o pipefail -o errtrace
+shopt -s inherit_errexit nullglob compat"${BASH_COMPAT=42}"
 
 function main() {
-  # app="commandunit"
-  # rc="$HOME/.${app}rc" && \
-  # target="$HOME/.bashrc" && \
-  # tag="#-${app^^}" && \
-  # cat <(curl "https://raw.githubusercontent.com/dakusui/commandunit/main/src/site/adoc/resources/examples/function_definition.rc") > "${rc}" && \
-  # sh -c 'grep "$4" "$1" >& /dev/null || printf "source $3$2$3 $4\n" >> "$1"' - "${target}" "${rc}" '"' "${tag}"
-
-  cat <(https://dakusui.github.io/commandunit/resources/examples/commandunit) > commandunit
-    if [ "${BASH_SOURCE[0]}" == "$0" ]; then
-    # If being executed.
-    rm -fr target commandunit-out
-    commandunit --clean -- "${@}"
+  [[ -e "./commandunit" ]] || {
+    cat <(curl https://raw.githubusercontent.com/dakusui/commandunit/wrapper-verified/commandunit) >commandunit
+    chmod 755 ./commandunit
+  }
+  if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+    ./commandunit --clean -- "${@}"
   else
-    # If being sourced.
-    export -f commandunit
-    echo "This file was sourced. Try 'commandunit --help' to see usage." >&2
+    echo "commandunit's wrapper script was downloaded to $(pwd)/commandunit. Place it under a directory on PATH environment variable." >&2
   fi
 }
 
